90a21a0e973e0bd1471a9b0262d8328b0293689d
Abdul Rehman <22sp-052-cs@students.uituniversity.edu.pk>
2024-10-03 02:45:13 +0500

    added calculator app

--- lib/eyes.dart
+++ lib/eyes.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class EyeWidget extends StatefulWidget {
+  final bool isEyeOpen;
+  const EyeWidget({Key? key, required this.isEyeOpen}) : super(key: key);
   @override
   EyeWidgetState createState() => EyeWidgetState();
 }
@@ -10,11 +12,12 @@ class EyeWidgetState extends State<EyeWidget> with SingleTickerProviderStateMixi
   late AnimationController _controller;
   late Animation<double> _animation;
 
+
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
-      duration: Duration(milliseconds: 500),
+      duration: Duration(milliseconds: 200),
       vsync: this,
     );
     _animation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller)
@@ -23,18 +26,49 @@ class EyeWidgetState extends State<EyeWidget> with SingleTickerProviderStateMixi
       });
   }
 
+  @override
+  void didUpdateWidget(covariant EyeWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    // Trigger open/close animation when the state changes
+    if (widget.isEyeOpen != oldWidget.isEyeOpen) {
+      if (widget.isEyeOpen) {
+        _controller.reverse(); // Open the eye
+      } else {
+        _controller.forward(); // Close the eye
+      }
+    }
+  }
+
   void blink() {
     if (!isBlinking) {
       setState(() {
         isBlinking = true;
       });
-      _controller.forward().then((_) {
-        _controller.reverse().then((_) {
+      if (widget.isEyeOpen) {
+        // If the eye is open, close it
+        _controller.forward().then((_) {
           setState(() {
+             // Eye is now closed
             isBlinking = false;
           });
         });
-      });
+      } else {
+        // If the eye is closed, open it
+        _controller.reverse().then((_) {
+          setState(() {
+             // Eye is now open
+            isBlinking = false;
+          });
+        });
+      }
+      // _controller.forward().then((_) {
+      //   _controller.reverse().then((_) {
+      //     setState(() {
+      //       isBlinking = false;
+      //     });
+      //   });
+      // });
     }
   }
 
@@ -82,13 +116,13 @@ class EyeWidgetState extends State<EyeWidget> with SingleTickerProviderStateMixi
           ),
           // Eyelid
           Positioned(
-            top: -50 + _animation.value * 50,
+            top: -10 + _animation.value * -80,
             child: Container(
               width: 100,
-              height: 50,
+              height: 100,
               decoration: BoxDecoration(
                 color: Colors.black,
-                borderRadius: BorderRadius.vertical(top: Radius.circular(50)),
+                borderRadius: BorderRadius.all(Radius.circular(50)),
               ),
             ),
           ),
