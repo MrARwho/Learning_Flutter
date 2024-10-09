de7ed51cc1cb37d525f3b96633b0cc994d2efec0
Abdul Rehman <22sp-052-cs@students.uituniversity.edu.pk>
2024-10-10 01:46:30 +0500

    added players 2 page app

--- lib/main.dart
+++ lib/main.dart
@@ -72,6 +72,23 @@ class Homepage extends StatelessWidget {
                 child: Text('Lab 2'),
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => PlayerList()));
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 255, 131, 82),
+                  textStyle: const TextStyle(fontSize: 25),
+                  padding: const EdgeInsets.all(20),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0)),
+                ),
+                child: Text('Lab 3'),
+              ),
+            ),
           ],
         ),
       ),
@@ -724,6 +741,93 @@ class CartItem extends StatelessWidget {
             icon: Icon(Icons.delete, color: Colors.red),
             onPressed: onDelete,
           ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class Players {
+  late String name;
+  late String type;
+  late int runs;
+  late int wickets;
+  late int matches;
+
+  Players({required this.name, required this.type}){
+    this.runs = 0;
+    this.wickets = 0;
+    this.matches = 0;
+
+  }
+}
+
+class PlayerList extends StatelessWidget {
+
+  final List<Players> players = [
+    Players(name: "Virat", type: "Batsman"),
+    Players(name: "bobzy", type: "Batsman"),
+    Players(name: "rizzu", type: "Batsman"),
+    Players(name: "Saad Akhtar", type: "Batsman"),
+    Players(name: "Rohit Sharma", type: "Batsman"),
+  ];
+  PlayerList({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Players'),
+      ),
+      body: ListView.builder(
+        itemCount: players.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => PlayerDetails(player: players[index]),
+                ),
+              );
+            },
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("${players[index].name.replaceFirst(" ", "_")}.png"),
+              radius: 30,
+            ),
+            title: Text(players[index].name),
+            subtitle: Text(players[index].type),
+          );
+        }
+      )
+    );
+    
+  }
+}
+
+class PlayerDetails extends StatelessWidget {
+  final Players player;
+  const PlayerDetails({super.key, required this.player});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(player.name),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage("${player.name.replaceFirst(" ", "_")}.png"),
+              radius: 100,
+            ),
+            SizedBox(height: 16),
+            Text("Runs: ${player.runs}"),
+            Text("Wickets: ${player.wickets}"),
+            Text("Matches: ${player.matches}"),
+          ],
         ),
       ),
     );
