2c713d72a9dfa1636eebe795f03d0f7b07344ab0
Abdul Rehman <22sp-052-cs@students.uituniversity.edu.pk>
2024-10-31 23:53:41 +0500

    added card

--- lib/card.dart
+++ lib/card.dart
@@ -15,47 +15,91 @@ class card extends StatelessWidget {
       ), //AppBar
       body: Center(
         child: Card(
-            elevation: 100,
-            shadowColor: Colors.black,
-            color: const Color.fromARGB(255, 10, 11, 41),
-            child: Container(
-              width: 300,
-              height: 500,
-              child: Column(
-              
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Abdul Rehman',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold),
+          elevation: 10,
+          shadowColor: Colors.black,
+          color: const Color.fromARGB(255, 10, 11, 41),
+          child: SizedBox(
+            width: 300,
+            height: 500,
+            child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 0.0),
+                    child: Text(
+                      'Abdul Rehman',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
                       ),
-                    ],
+                    ),
                   ),
-
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        '0004-2520-0000-1231',
-                        style: TextStyle(
-                            color: const Color.fromARGB(255, 219, 136, 20),
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold),
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 80.0),
+                    child: Text(
+                      '0004-2520-0000-1231',
+                      style: TextStyle(
+                        color: const Color.fromARGB(255, 219, 136, 20),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
                       ),
-                    ],
-                  )
-                ],
-              ),
-            )
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 40.0, right: 10.0),
+                    child: Text(
+                      'Expiry: 22/5/2027',
+                      style: TextStyle(
+                        color: const Color.fromARGB(255, 255, 255, 255),
+                        fontSize: 15,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 0.0, left: 10.0),
+                    child: Image(image: const AssetImage('visa.png'),
+                      height: 100,
+                      width: 100,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 0.0, left:5.0),
+                    child: Image(image: const AssetImage('AR.png'),
+                      height: 80,
+                      width: 80,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 0.0, left:5.0),
+                    child: Image(image: const AssetImage('NFC.png'),
+                      height: 80,
+                      width: 80,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-      )
     ); //Center
-    
   }
 }
